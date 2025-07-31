e266e3b890f19315319ae721a836e0822259b896
SaikiranG667 <rayquazasai@gmail.com>
2025-07-31 11:10:28 +0530

    Second Milestone

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,4 +11,14 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch products" });
   }
 });
+
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findOne({ id: req.params.id });
+    if (!product) return res.status(404).json({ error: "Product not found" });
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: "Error fetching product" });
+  }
+});
 module.exports = router;
